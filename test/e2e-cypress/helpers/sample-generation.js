0d88d6191fe0b4b7b48eb349580e59d087e80b23
mathis-m <mathis.michel@outlook.de>
2020-10-08 10:48:13 +0200

    add test base

---
+++ test/e2e-cypress/helpers/sample-generation.js
@@ -0,0 +1,3 @@
+export const checkIsValue = (value) => {
+
+}
---
+++ test/e2e-cypress/tests/features/straighten-sample.js
@@ -0,0 +1,11 @@
+describe("Straighten Sample generation", () => {
+  describe("Json Sample Generator", () => {
+    describe("Merge example with schema sample", () => {
+      describe("Case primitive schema type", () => {
+        it("should use own example if exists", () => {
+
+        })
+      })
+    })
+  })
+})
