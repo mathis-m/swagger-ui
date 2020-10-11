3b0b7b3f7d0d33cae095d97eadc17a028062bd16
mathis-m <mathis.michel@outlook.de>
2020-10-11 15:33:40 +0200

    add primitive test structure

--- test/e2e-cypress/tests/features/straighten-sample.js
+++ test/e2e-cypress/tests/features/straighten-sample.js
@@ -1,8 +1,27 @@
-describe("Straighten Sample generation", () => {
-  describe("Json Sample Generator", () => {
-    describe("Merge example with schema sample", () => {
-      describe("Case primitive schema type", () => {
-        it("should use own example if exists", () => {
+describe("Straighten sample generation", () => {
+  describe("Json sample generator", () => {
+    describe("Case primitive schema type", () => {
+      describe("Override example", () => {
+        it("should use override example if exists.", () => {
+
+        })
+        it("should use override example over schema's example.", () => {
+
+        })
+        it("should use override example over generated sample of schema.", () => {
+
+        })
+      })
+      describe("Schema's Example", () => {
+        it("should be used if exists", () => {
+
+        })
+        it("should use schema's example over generated sample of schema.", () => {
+
+        })
+      })
+      describe("Generated schema's sample", () => {
+        it("should be generated when no override and no example provided", () => {
 
         })
       })
